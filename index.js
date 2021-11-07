339237451459115ee927dcb3c4b079535b4a14c9
arczen <71906782+arc-zen@users.noreply.github.com>
2021-11-06 18:50:00 -1000

    11/6/21 6:49 PM

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 function copyContacts(c) {
 	navigator.clipboard.writeText(c);
 }
-document.getElementById('cc').addEventListener('click', function (event) {
-    console.log("copied successfully");
-    copyContacts("arczen#7561");
-    cc.innerHTML = "Copied!";
-	setTimeout(function () {
-		cc.innerHTML = "Contact";
-	}, 1000);
+// document.getElementById('cc').addEventListener('click', function (event) {
+//     console.log("copied successfully");
+//     copyContacts("arczen#7561");
+//     cc.innerHTML = "Copied!";
+// 	setTimeout(function () {
+// 		cc.innerHTML = "Contact";
+// 	}, 1000);
+// })
+document.getElementById("name").addEventListener("mouseover", function(event) {
+	var name = document.getElementById("name")
+	name.innerHTML = "arczen#7561"
+	setTimeout(() => {name.innerHTML = "&lt;/arczen>"}, 3000)
 })
